202e724a576c0b4d8b6aa652971631b5439b97ce
AVogelsanger <voguednj@hotmail.com>
2025-08-12 17:50:09 -0300

    video 07: RequestMapping e Servlet Context

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -1,12 +1,14 @@
 package academy.devdojo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HelloController {
 
-    @GetMapping("hi/")
+    @GetMapping("hi")
     public String hi() {
         return "OMAE WA MOU SHINEO IRU";
     }
